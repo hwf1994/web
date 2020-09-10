662afb2d33aef48b698ca7216f37ac05f705c251
何文飞 <hewenfei@yolanda.hk>
2020-09-10 16:32:49 +0800

    初次提交

---
+++ common/js/aside.js
@@ -0,0 +1,13 @@
+document.writeln('<aside>');
+document.writeln('    <ul>');
+document.writeln('        <li><a>小程序购课</a></li>');
+document.writeln('        <li><a>微信公众号</a></li>');
+document.writeln('        <li><a>电话咨询</a></li>');
+document.writeln('        <li><a class="returnTop">返回顶部</a></li>');
+document.writeln('    </ul>');
+document.writeln('</aside>');
+
+$(".returnTop").click(function(){
+    console.log(123)
+    $(window).scrollTop(0)
+})
